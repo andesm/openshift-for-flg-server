078a8f115f04c0098c2ace5689a3c74816d7bc92
MIYATA Tadaaki <andes@flg.jp>
2020-03-08 11:01:10 +0900

    [ADD] mattermost

--- all-cleanup.sh
+++ all-cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 
 ./delete.sh
+oc get pods | egrep 'No resources found.'
+while [ $? = 0 ]
+do
+    sleep 5
+    oc get pods | grep 'fx-system-trade.*Terminating'
+done
 ./prune.sh
 ./create.sh
 ./scaledown.sh
